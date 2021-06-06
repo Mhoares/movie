ca284794890ca5aca5f1b45c3d97a85c85310fb9
Mario Hoare Silva <mario.hoare@sansano.usm.cl>
2021-06-06 13:52:21 -0400

    reparada fallas del interceptor

--- src/app/shared/jwt-interceptor.interceptor.ts
+++ src/app/shared/jwt-interceptor.interceptor.ts
@@ -20,24 +20,39 @@ export class JwtInterceptorInterceptor implements HttpInterceptor {
     const currenTokens :any = this.auth.currenTokensValue;
     const isAPI = request.url.startsWith("http://161.35.140.236:9005");
     const isRefresh = request.url =="http://161.35.140.236:9005/api/auth/refresh"
+    const isLogin = request.url =="http://161.35.140.236:9005/api/auth/login"
     
     if (currenTokens && isAPI && !isRefresh ) {
 
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${currenTokens.token}`
-        }
-      });
+      
+      if(this.auth.isTokenExpired() && !isLogin)
+      return this.auth.getRefreshToken().pipe(
+        mergeMap(() =>{
+         request = this.cloneRequestAndSetToken(request, this.auth.currenTokens)
+         return next.handle(request)
+        }),
+        catchError(() =>{
+          this.router.navigate(['/login'])
+          return throwError("No se pudo refrescar el token")
+        })
+      )
+      else if(!isLogin){
+         request = this.cloneRequestAndSetToken(request, currenTokens)
+        return next.handle(request)
+      }else{
+        return next.handle(request)
+      }
+    }else{
+     return next.handle(request)
     }
-    if(this.auth.isTokenExpired()  && !isRefresh)
-    return this.auth.getRefreshToken().pipe(
-      mergeMap(() =>next.handle(request)),
-      catchError(() =>{
-        this.router.navigate(['/login'])
-        return throwError("No se pudo refrescar el token")
-      })
-    )
-    else
-    return next.handle(request);
+
+  }
+  private cloneRequestAndSetToken(request, currenTokens){
+    
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${currenTokens.token}`
+      }
+    });
   }
 }
